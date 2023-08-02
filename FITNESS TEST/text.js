3c1920e9dc83848393662ac9dd0b92889580d593
Precious Osazenaye <101315723+Preciousosas1@users.noreply.github.com>
2023-08-02 12:04:39 +0100

    Add files via upload
    HTTA Fitness test 2023

---
+++ FITNESS TEST/text.js
@@ -0,0 +1,120 @@
+const color = document.getElementById("addons");
+
+ color.addEventListener("click",(event) =>{
+   event.target.style.background = "purple";
+   event.target.style.color = "white";
+
+ })
+
+ //image 2
+
+ const col = document.getElementById("addons2");
+
+ col.addEventListener("click",(event) =>{
+   event.target.style.background = "blue";
+   event.target.style.color = "yellow";
+
+ })
+
+ //image3
+
+ const cols = document.getElementById("addons3");
+
+ cols.addEventListener("click",(event) =>{
+   event.target.style.background = "green";
+   event.target.style.color = "yellow";
+
+ })
+
+ //image4
+
+ const cole = document.getElementById("addons4");
+
+ cole.addEventListener("click",(event) =>{
+   event.target.style.background = "black";
+   event.target.style.color = "greenyellow";
+
+ })
+
+
+ //image5
+
+ const collection = document.getElementById("addons5");
+
+ collection.addEventListener("click",(event) =>{
+   event.target.style.background = "yellow";
+   event.target.style.color = "black";
+
+ })
+
+
+ //image6
+
+ const collect = document.getElementById("addons6");
+
+ collect.addEventListener("click",(event) =>{
+   event.target.style.background = "red";
+   event.target.style.color = "white";
+
+ })
+
+ //image7
+
+ const coled = document.getElementById("addons7");
+
+ coled.addEventListener("click",(event) =>{
+   event.target.style.background = "black";
+   event.target.style.color = "skyblue";
+
+ })
+
+ //image8
+
+ const x = document.getElementById("addons8");
+
+ x.addEventListener("click",(event) =>{
+   event.target.style.background = "orange";
+   event.target.style.color = "white";
+
+ })
+
+ //image 9
+
+ const xxx = document.getElementById("addons9");
+
+ xxx.addEventListener("click",(event) =>{
+   event.target.style.background = "pink";
+   event.target.style.color = "black";
+
+ })
+
+
+ //image 10
+
+ const y = document.getElementById("addons10"); 
+ y.addEventListener("click",(event) =>{
+   event.target.style.background = "black";
+   event.target.style.color = "aqua";
+
+ })
+
+
+ //image 11
+
+ const yy = document.getElementById("addons11");
+
+ yy.addEventListener("click",(event) =>{
+   event.target.style.background = "red";
+   event.target.style.color = "white";
+
+ })
+
+ //image 12
+
+ const yyy = document.getElementById("addons12");
+
+ yyy.addEventListener("click",(event) =>{
+   event.target.style.background = "goldenrod";
+   event.target.style.color = "black";
+
+ })
